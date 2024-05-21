768325f3930200e247bdd948f2f0fdac4a77cb00
ysm-dev <i@ysm.dev>
2024-05-21 11:42:41 +0900

    u

--- utils/getBaseURL.tsx
+++ utils/getBaseURL.tsx
@@ -2,7 +2,5 @@ import { HOST } from 'lib/urls'
 import { isProd } from 'utils/isProd'
 
 export const getBaseURL = () => {
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
-
   return isProd() ? HOST : `http://localhost:${process.env.PORT ?? 3000}`
 }
